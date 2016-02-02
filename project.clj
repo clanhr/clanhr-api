fc214eea60a45aa8ad6c0ffd8f5c4e3b2f2b9a1a
Pedro Santos <donbonifacio@gmail.com>
2016-02-02 09:46:44 +0000

    Add error message to generic exception
    Bugsnag isn't showing inner exception. Trying to add more info (maybe
    should just use original exception?).

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/clanhr-api "1.7.1"
+(defproject clanhr/clanhr-api "1.7.2"
   :description "Raw clojure interface to ClanHR's APIs"
   :url "https://github.com/clanhr/clanhr-api"
   :dependencies.edn "https://raw.githubusercontent.com/clanhr/dependencies/master/dependencies.edn"
